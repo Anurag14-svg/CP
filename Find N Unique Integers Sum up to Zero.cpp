56238cb5eae6b774b81d42ef5fb0d40c2368013d
Anurag14-svg <84181319+Anurag14-svg@users.noreply.github.com>
2022-02-03 07:32:40 -0800

    Update Find N Unique Integers Sum up to Zero.cpp

--- Find N Unique Integers Sum up to Zero.cpp
+++ Find N Unique Integers Sum up to Zero.cpp
@@ -17,3 +17,17 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+    public int[] sumZero(int n) {
+        int arr[] = new int[n];
+        int sum = 0;
+        for(int i = 0; i < n-1; i++) {
+            arr[i] = i+1;
+            sum += arr[i];
+        }
+        arr[n-1] = -sum;
+        return arr;
+    }
+}
